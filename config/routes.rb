e650c60874b98689b6e829c5f179dd59d1459b8f
HankLiu5 <liu19901124@gmail.com>
2015-11-17 21:48:48 -0800

    Setup notes controller and nest notes inside the books

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :books
+  resources :books do
+    # Nest notes inside books. Paths to notes resources will have book id in them.
+    # The functions we need is create and destroy because we will put these into books#show.
+    resources :notes, only: [:create, :destroy]
+  end
   # set up the root
   root to: "books#index"
 end
