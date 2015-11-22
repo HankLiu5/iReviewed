87508764738ec5a901a0223bb5cd376fb7ac5847
HankLiu5 <liu19901124@gmail.com>
2015-11-21 18:43:39 -0800

    Create session controller.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  # We can think of new action as login page and destroy as logout page.
+  # Need new and create actions to create a session.
+  def new
+  end
+
+  def create
+  end
+
+  # Need a destroy action to destroy a session.
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  resources :sessions, only: [:new, :create, :destroy]
   resources :books do
     # Nest notes inside books. Paths to notes resources will have book id in them.
     # The functions we need is create and destroy because we will put these into books#show.
@@ -6,4 +8,11 @@ Rails.application.routes.draw do
   end
   # set up the root
   root to: "books#index"
+
+  # When I have a get request to a "/login" page, that should be interpreted as
+  # something that should go to sessions controller new action.
+  # And, in our application, we're going to refer to that as login.
+  # As a result, we will have login_path or login_url.
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
 end
